43af99b8b0be6d0c809eabfe61fbeee44f1ed8a8
Jian Ma <jianquanma@gmail.com>
2024-12-17 23:30:22 -0500

    Initial Commit

---
+++ Odola/OdolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdolaApp.swift
+//  Odola
+//
+//  Created by Jian Ma on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct OdolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
